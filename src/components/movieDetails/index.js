4445ec135471bd165e31eb98c2b749d0f35d4d1e
Eoin Hearne <eoin.hearne@errigal.com>
2021-03-11 16:22:49 +0000

    Show movie review excerpts

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -8,6 +8,8 @@ import NavigationIcon from "@material-ui/icons/Navigation";
 import Fab from "@material-ui/core/Fab";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from "../movieReviews";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,9 +30,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieDetails = ( props) => {
+const MovieDetails = ({ movie }) => {
   const classes = useStyles();
-  const movie = props.movie
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => {
+    return () => setDrawerOpen(open);
+  };
+
 
   return (
     <>
@@ -78,11 +85,15 @@ const MovieDetails = ( props) => {
       <Fab
         color="secondary"
         variant="extended"
+        onClick={toggleDrawer(true)}
         className={classes.fab}
       >
         <NavigationIcon />
         Reviews
       </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
       </>
   );
 };
---
+++ src/util.js
@@ -0,0 +1,9 @@
+import truncate from "lodash/truncate";
+
+export function excerpt(string) {
+  // const truncate = truncate;
+  return truncate(string, {
+    length: 400, // maximum 400 characters
+    separator: /,?\.* +/, // separate by spaces, including preceding commas and periods
+  });
+}
