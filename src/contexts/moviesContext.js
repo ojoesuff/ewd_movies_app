6c34a4d5ef56170d9bf89317a29f45f63f23f000
Eoin Hearne <eoin.hearne@errigal.com>
2021-03-18 15:35:02 +0000

    Refactor movies context to use useReducer hook

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -1,32 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import { getMovies } from "../api/tmdb-api";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-favorite":
+      return {
+        movies: state.movies.map((m) =>
+          m.id === action.payload.movie.id ? { ...m, favorite: true } : m
+        ),
+      };
+    case "remove-favorite":
+      return {
+        movies: state.movies.map((m) =>
+          m.id === action.payload.movie.id ? { ...m, favorite: false } : m
+        ),
+      };
+    case "load-discover-movies":
+      return {
+        movies: action.payload.movies,
+      };
+    default:
+      return state;
+  }
+};
 
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
-    const [movies, setMovies] = useState([]);
-
-    const removeFromFavorites = (movieId) => {
-        setMovies((movies) => {
-        const updatedMovies = movies.map((m) =>
-            m.id === movieId ? { ...m, favorite: false } : m
-        );
-        return updatedMovies;
-        });
-    };
+  const [state, dispatch] = useReducer(reducer, { movies: [] });
 
   const addToFavorites = (movieId) => {
-    setMovies((movies) => {
-      const updatedMovies = movies.map((m) =>
-        m.id === movieId ? { ...m, favorite: true } : m
-      );
-      return updatedMovies;
+    const index = state.movies.map((m) => m.id).indexOf(movieId);
+    dispatch({ type: "add-favorite", payload: { movie: state.movies[index] } });
+  };
+
+  const removeFromFavorites = (movieId) => {
+    const index = state.movies.map((m) => m.id).indexOf(movieId);
+    dispatch({
+      type: "remove-favorite",
+      payload: { movie: state.movies[index] },
     });
   };
 
   useEffect(() => {
     getMovies().then((movies) => {
-      setMovies(movies);
+      dispatch({ type: "load-discover-movies", payload: { movies } });
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -34,7 +52,7 @@ const MoviesContextProvider = (props) => {
   return (
     <MoviesContext.Provider
       value={{
-        movies: movies,
+        movies: state.movies,
         addToFavorites: addToFavorites,
         removeFromFavorites: removeFromFavorites,
       }}
